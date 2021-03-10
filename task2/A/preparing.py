0f90f7529374f33c3c0ab351efceedaeb9550849
Kirill <kirill.grachoff@gmail.com>
2021-03-10 17:03:56 +0300

    changes in CMakeLists and libraries

--- task2/A/preparing.py
+++ task2/A/preparing.py
@@ -3,5 +3,5 @@ import sys
 if __name__ == '__main__':
     output_file = sys.argv[1]
     with open(output_file, 'w') as fp:
-        fp.write("#pragma once\nbool checkIfCorrect() { return true; }\n")
+        fp.write("#pragma once\ninline bool checkIfCorrect() { return true; }\n")
         fp.close()
